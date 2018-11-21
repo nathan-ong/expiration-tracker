2169fc3f0a9bf0baae917b6b57b94bf053d1c731
Nathan Ong <nathan.d.ong@gmail.com>
2018-11-20 17:15:35 -0800

    Remove commented out text

--- client/src/components/RenderedInfo.jsx
+++ client/src/components/RenderedInfo.jsx
@@ -1,28 +1,3 @@
-// import React, { Component } from 'react'
-
-// export default function RenderedInfo() {
-//   // render() {
-//     return (
-//       <div>
-//         <span>Item: {this.props.itemName}</span>
-//         <br />
-//         Expiration
-//         <br />
-//         <span>Day: {this.props.expDay}</span>
-//         <br />
-//         <span>Month: {this.props.expMonth}</span>
-//         <br />
-//         <span>Year: {this.props.expYear}</span>
-//         <br />
-//         <br />
-//         Remind me
-//         <br />
-//         <span>{`${this.props.reminderNum} ${this.props.reminderUnit}`}</span>
-//         <span> before expiration</span>
-//       </div>
-//     )
-//   // }
-// }
 import React from 'react'
 
 export default function RenderedInfo(props) {
